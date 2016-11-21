c699f52b8b71c302d1aaa57e33bca967968ce0fd
Robin Laugs <Laugslander@users.noreply.github.com>
2016-11-21 20:02:41 +0100

    Added the required Soccar library classes. These classes will be replaced by a library JAR in the future.

---
+++ Soccar [Main-Server]/src/nl/soccar/library/enumeration/Privilege.java
@@ -0,0 +1,13 @@
+package nl.soccar.library.enumeration;
+
+/**
+ * Enumeration of all available Player privileges in the game.
+ * 
+ * @author PTS34
+ */
+public enum Privilege {
+    
+    NORMAL,
+    ADMINISTRATOR
+    
+}
