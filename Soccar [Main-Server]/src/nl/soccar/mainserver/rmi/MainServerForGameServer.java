9d20df96995aa414b84077ae3d9b5bdc1bc0e1b6
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2016-12-06 11:03:01 +0100

    Fix merge conflicts...

--- Soccar [Main-Server]/src/nl/soccar/mainserver/rmi/MainServerForGameServer.java
+++ Soccar [Main-Server]/src/nl/soccar/mainserver/rmi/MainServerForGameServer.java
@@ -52,6 +52,16 @@ public class MainServerForGameServer extends GeneralMainServer implements IMainS
     }
 
     @Override
+    public void increaseSessionOccupancy(IGameServerForMainServer gameServer, SessionData sessionData) throws RemoteException {
+
+    }
+
+    @Override
+    public void decreaseSessionOccupancy(IGameServerForMainServer gameServer, SessionData sessionData) throws RemoteException {
+
+    }
+
+    @Override
     public void addGoals(String username, int goals) throws RemoteException {
         super.getStatisticsRepository().addGoals(username, goals);
     }
