3b40b5bf8c5b397578c0fc5c837957df8692e9c3
Limpens,Raymond R.H.B. <Ranray@users.noreply.github.com>
2017-01-10 09:30:35 +0100

    Added tests for the PasswordUtilities class.

---
+++ Soccar [Main-Server]/test/nl/soccar/mainserver/util/test/PasswordUtilitiesTest.java
@@ -0,0 +1,43 @@
+package nl.soccar.mainserver.util.test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import nl.soccar.mainserver.util.PasswordUtilities;
+import org.junit.Test;
+
+/**
+ * JUnit test that tests the nl.soccar.mainserver.util.PaswordUtilities class.
+ *
+ * @author PTS34A
+ */
+public class PasswordUtilitiesTest {
+
+    private static final int DEFAULT_SALT_LENGTH = 16;
+    private static final int HASHED_PASSWORD_LENGTH = 64;
+    
+    private final String password = "Welkom123";
+
+    @Test
+    public void generatingSaltTest() {
+        byte[] salt = PasswordUtilities.generateSalt();
+        // Checks if the salt is not null.
+        assertNotNull(salt);
+        // Checks if the salt is 16 long.
+        assertEquals(DEFAULT_SALT_LENGTH, salt.length);
+    }
+    
+    @Test
+    public void hashingTest() {
+        byte[] salt = PasswordUtilities.generateSalt();
+        // Checks if the salt is not null.
+        assertNotNull(salt);
+        // Checks if the salt is 16 bytes long.
+        assertEquals(DEFAULT_SALT_LENGTH, salt.length);
+        
+        byte[] hashedPassword = PasswordUtilities.addSalt(password.getBytes(), salt);
+        //Checks if the hashedPassword is not null
+        assertNotNull(hashedPassword);
+        // Checks if the hashedpassword is 64 bytes long.
+        assertEquals(HASHED_PASSWORD_LENGTH, hashedPassword.length);
+    }
+}
